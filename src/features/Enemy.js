1b16a1bad8da37e317a378446a60519149edfdd0
Khadison <Khadisonchin@gmail.com>
2023-03-19 17:32:11 -0700

    Finished with Enemy class

--- src/features/Enemy.js
+++ src/features/Enemy.js
@@ -1,7 +1,24 @@
+const e = require("express");
+
 class Enemy {
-  name;
-  healthAmount;
-  worth;
+  constructor(name, healthAmount, speed, reward, lifeDamage) {
+    this.name = name;
+    this.healthAmount = healthAmount;
+    this.speed = speed;
+    this.reward = reward;
+    this.lifeDamage = lifeDamage
+  }
+
+  losingHealth(towerDamage) {
+    this.healthAmount -= towerDamage
+  }
+
+  /*gainingGold(totalMoney) {
+    this.reward += totalMoney
+  }*/
 }
+
+const scorpion = new Enemy('Scorpion', 100, 10, 10, 1);
+const air = new Enemy('Flying bug', 100, 10, 10, 1);
 
 module.exports = Enemy;
