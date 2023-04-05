0e1ba94d0100d1a58ab0ec4f7a42522edee8fba9
Jason Nguyen <jnguyen7539@gmail.com>
2023-04-04 18:27:16 -0700

    updated economy to use es module. starting calling from main scene

--- src/features/economy.js
+++ src/features/economy.js
@@ -1,21 +1,33 @@
-class Economy {
+// TODO: use this as a es module example
+
+export default class Economy {
   // design instance variable
   totalMoney;
+  currentScene;
+  goldName;
+  goldText;
 
-  constructor(startingMoney) {
+  constructor(startingMoney, scene, goldName) {
     this.totalMoney = startingMoney;
+    this.currentScene = scene;
+    this.goldName = goldName;
+  }
+
+  render() {
+    // TODO: add paramters to bring this to the front
+    this.currentScene.add.image(30, 25, this.goldName);
+    this.currentScene.add.text(65, 15, `=${this.totalMoney}`, {
+      fontSize: "32px",
+      fill: "#000",
+    });
   }
 
   // designing methods
   addMoney(amount) {
     this.totalMoney = this.totalMoney + amount;
+    this.render();
   }
   subtractMoney(amount) {
     this.totalMoney = this.totalMoney - amount;
   }
 }
-
-module.exports = Economy;
-
-Economy.addMoney(100)
- console.log(Economy.totalMoney);
--- src/features/mainScene.js
+++ src/features/mainScene.js
@@ -1,5 +1,7 @@
 // This is the entry point of your game.
 
+import Economy from "./economy.js";
+
 const width = 1024;
 const height = 1024;
 
@@ -7,33 +9,34 @@ const config = {
   width,
   height,
   type: Phaser.AUTO,
-  backgroundColor: '#4488aa',
+  backgroundColor: "#4488aa",
   scene: { preload, create },
 };
 
 const game = new Phaser.Game(config);
 
 function preload() {
-  this.load.tilemapTiledJSON('map', 'map01.json');
-  this.load.image('tiles', 'Grass_Tileset.png');
+  this.load.tilemapTiledJSON("map", "map01.json");
+  this.load.image("tiles", "Grass_Tileset.png");
   this.load.image("play_button", "play_button.png");
   this.load.image("gold", "gold.png");
-  this.load.atlas('scorpion', 'Scorpion.png', 'Scorpion.json');
-
+  this.load.atlas("scorpion", "Scorpion.png", "Scorpion.json");
 }
 
 //Gold bar on hud
-var gold = 100
-var goldImage
+var gold = 100;
+var goldImage;
 
 //Level needs to change when all enemies are dead
-var level = 1
-var levelText
+var level = 1;
+var levelText;
 
-var lives = 50
-var livesText
+var lives = 50;
+var livesText;
 
 function create() {
+  let econ = new Economy(0, this, "gold");
+
   const centerX = width / 2;
   const centerY = height / 2;
 
@@ -43,42 +46,80 @@ function create() {
 
   playButton.on("pointerdown", () => {
     alert("clicked button");
-
   });
 
   playButton.setDepth(1);
 
   //background of game
-  const map = this.make.tilemap({ key: 'map' });
-  const tileset = map.addTilesetImage("Grass_Tileset", 'tiles');
-  const layer = map.createLayer('Background', tileset);
+  const map = this.make.tilemap({ key: "map" });
+  const tileset = map.addTilesetImage("Grass_Tileset", "tiles");
+  const layer = map.createLayer("Background", tileset);
 
   //Object layer in tiled called Start
-  const startPointLayer = map.getObjectLayer('Start');
+  const startPointLayer = map.getObjectLayer("Start");
   //Object layer in tiled called End
-  const endPointLayer = map.getObjectLayer('End');
+  const endPointLayer = map.getObjectLayer("End");
 
   //This is to get x and y for enemies pathing (maybe change startPointObject name)
-  const startPointObject = startPointLayer.objects.find(object => object.properties.find(prop => prop.name === 'StartPoint').value === '200');
-  const endPointObject = endPointLayer.objects.find(object => object.properties.find(prop => prop.name === 'EndPoint').value === '200');
+  const startPointObject = startPointLayer.objects.find(
+    (object) =>
+      object.properties.find((prop) => prop.name === "StartPoint").value ===
+      "200"
+  );
+  const endPointObject = endPointLayer.objects.find(
+    (object) =>
+      object.properties.find((prop) => prop.name === "EndPoint").value === "200"
+  );
 
   //Pathing
   const path = new Phaser.Curves.Path();
   path.moveTo(startPointObject.x, startPointObject.y);
   path.lineTo(endPointObject.x, endPointObject.y);
-  const startPoint = new Phaser.Math.Vector2(startPointObject.x, startPointObject.y);
+  const startPoint = new Phaser.Math.Vector2(
+    startPointObject.x,
+    startPointObject.y
+  );
   const endPoint = new Phaser.Math.Vector2(endPointObject.x, endPointObject.y);
 
   //Animations
-  this.anims.create({ key: 'moving_left', frames: this.anims.generateFrameNames('scorpion', { prefix: 'Walk', end: 7, zeroPad: 3 }), repeat: -1 });
-  this.anims.create({ key: 'up', frames: this.anims.generateFrameNames('scorpion', { prefix: 'U', end: 7, zeroPad: 3 }), repeat: -1 });
-  this.anims.create({ key: 'down', frames: this.anims.generateFrameNames('scorpion', { prefix: 'D', end: 7, zeroPad: 3 }), repeat: -1 });
+  this.anims.create({
+    key: "moving_left",
+    frames: this.anims.generateFrameNames("scorpion", {
+      prefix: "Walk",
+      end: 7,
+      zeroPad: 3,
+    }),
+    repeat: -1,
+  });
+  this.anims.create({
+    key: "up",
+    frames: this.anims.generateFrameNames("scorpion", {
+      prefix: "U",
+      end: 7,
+      zeroPad: 3,
+    }),
+    repeat: -1,
+  });
+  this.anims.create({
+    key: "down",
+    frames: this.anims.generateFrameNames("scorpion", {
+      prefix: "D",
+      end: 7,
+      zeroPad: 3,
+    }),
+    repeat: -1,
+  });
 
   //Sprite or animation for pathing
-  const follower = this.add.follower(path, startPointObject.x, startPointObject.y, 'scorpion');
+  const follower = this.add.follower(
+    path,
+    startPointObject.x,
+    startPointObject.y,
+    "scorpion"
+  );
   follower.startFollow({
     duration: 10000,
-    ease: 'Linear',
+    ease: "Linear",
     anims: this.anims,
   });
 
@@ -95,24 +136,23 @@ function create() {
       const { x, y } = follower;
       const angle = Phaser.Math.Angle.BetweenPoints(startPoint, endPoint);
 
-      if ((angle > -Math.PI && angle <= -3 * Math.PI / 4) || (angle >= Math.PI / 4 && angle <= Math.PI)) { 
-        follower.anims.play('moving_left', true);
+      if (
+        (angle > -Math.PI && angle <= (-3 * Math.PI) / 4) ||
+        (angle >= Math.PI / 4 && angle <= Math.PI)
+      ) {
+        follower.anims.play("moving_left", true);
         follower.setFlipX(true);
-       
-      } else if (angle > Math.PI / 4 && angle <= 3 * Math.PI / 4) {
-        follower.anims.play('down', true);
+      } else if (angle > Math.PI / 4 && angle <= (3 * Math.PI) / 4) {
+        follower.anims.play("down", true);
         follower.setFlipX(false);
-        
-      } else if (angle > -3 * Math.PI / 4 && angle <= -Math.PI / 4) {
-        follower.anims.play('moving_left', true);
+      } else if (angle > (-3 * Math.PI) / 4 && angle <= -Math.PI / 4) {
+        follower.anims.play("moving_left", true);
         follower.setFlipX(false);
-        
       } else {
-        follower.anims.play('up', true);
+        follower.anims.play("up", true);
         follower.setFlipX(false);
-        
       }
-    }
+    },
   });
 
   //Use these to see if it is working or getting x and y
@@ -120,16 +160,16 @@ function create() {
   console.log("startPointObject.y = " + startPointObject.y);
   console.log(startPointLayer);
 
-  goldImage = this.add.image(30, 25, 'gold');
-  goldImage = this.add.text(65, 15, '=0', { fontSize: '32px', fill: '#000', });
+  // TODO: call when we create economy. don't reliant on call order.( bring the level of goldImage and gold text to the top of scene)
+  econ.render();
 
   //Maybe change fontFamily later
-  levelText = this.add.text(425, 15, 'Level:1/5', { fontSize: '32px', fill: '#000', });
-  livesText = this.add.text(812, 15, 'Lives:50', { fontSize: '32px', fill: '#000', });
-
-
+  levelText = this.add.text(425, 15, "Level:1/5", {
+    fontSize: "32px",
+    fill: "#000",
+  });
+  livesText = this.add.text(812, 15, "Lives:50", {
+    fontSize: "32px",
+    fill: "#000",
+  });
 }
-
-
-
-
